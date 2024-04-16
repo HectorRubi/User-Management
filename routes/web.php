54ca0ef6b2a4fd3b49ccee3368d7c89e580f2363
Hector Rubi <hector.rubi.garcia@outlook.com>
2024-04-15 19:49:38 -0600

    Modify root route to redirect to login

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Auth/Login', [
-        'canResetPassword' => Route::has('password.request'),
-        'canRegister' => Route::has('register'),
-        'status' => session('status'),
-    ]);
-});
+Route::redirect('/', '/login');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
